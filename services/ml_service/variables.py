1e31d6cf619ffc4574493202f2b5a58252687435
ozerge <ozerge@yandex.ru>
2025-08-01 02:22:08 +0000

    services_from_fast_api

---
+++ services/ml_service/variables.py
@@ -0,0 +1,17 @@
+MODEL_PATH = '../models/fitted_model_cbr_l.pkl'
+REQUIRED_MODEL_PARAMS = [
+    'build_year',
+    'building_type_int',
+    'latitude',
+    'longitude',
+    'ceiling_height',
+    'flats_count',
+    'floors_total',
+    'has_elevator',
+    'floor',
+    'kitchen_area',
+    'living_area',
+    'rooms',
+    'is_apartment',
+    'total_area'
+]
